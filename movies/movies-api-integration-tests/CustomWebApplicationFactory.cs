03d6582ee20ad170e69318519b9c5b56733e2457
Sonal Satpute <sonal.satpute@gmail.com>
2023-12-14 12:44:35 +0530

    Add integration tests for Movies API
    Created a new project `movies-api-integration-tests` to hold integration tests for the Movies API. Integration testing infrastructure is configured with necessary dependencies for test execution. Included a sample test for the Movies Controller to assert basic functionality.

---
+++ movies/movies-api-integration-tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using movies_api.infrastructures.persistent;
+using movies_api.services;
+
+namespace movies_api_integration_tests;
+
+internal class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly string _connectionString;
+
+    public CustomWebApplicationFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var service = services.SingleOrDefault(d => d.ServiceType == typeof(MoviesService));
+            services.Remove(service!);
+
+            services.Configure<MoviesDatabaseSettings>(options =>
+            {
+                options.ConnectionString = _connectionString;
+                options.DatabaseName = "moviesdb";
+                options.MoviesCollectionName = "Movies";
+            });
+
+            services.AddSingleton<MoviesService>();
+        });
+        builder.UseEnvironment("Development");
+    }
+
+    private static void InitDatabase(string connectionString, string databaseName, string collectionName)
+    {
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase(databaseName);
+        _ = database.GetCollection<BsonDocument>(collectionName);
+    }
+}
---
+++ movies/movies-api-integration-tests/MoviesControllerTests.cs
@@ -0,0 +1,46 @@
+using FluentAssertions;
+using Testcontainers.MongoDb;
+
+namespace movies_api_integration_tests;
+
+[TestFixture]
+internal class MoviesControllerTests
+{
+    private CustomWebApplicationFactory _factory = null!;
+    private HttpClient _client = null!;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        var container = new MongoDbBuilder()
+            .WithImage("mongo:latest")
+            .Build();
+
+        await container.StartAsync();
+        string connectionString = container.GetConnectionString();
+
+        _factory = new CustomWebApplicationFactory(connectionString);
+        _client = _factory.CreateClient();
+        _client.Timeout = TimeSpan.FromMinutes(2); // Set timeout to 2 minutes
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        _factory.Dispose();
+        _client.Dispose();
+    }
+    
+    
+    [Test]
+    public async Task METHOD()
+    {
+        // Arrange
+        var responseMessage = await _client.GetAsync("api/Movies");
+        
+        // Act
+        responseMessage.EnsureSuccessStatusCode();
+        var response = await responseMessage.Content.ReadAsStringAsync();
+        response.Should().NotBeNullOrEmpty();
+    }
+}
